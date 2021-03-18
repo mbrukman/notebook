2560099efa1f86c33c37bc13cfd12bc970ab36d4
Misha Brukman <mbrukman@google.com>
2021-03-18 19:07:11 -0400

    Add a proxy to mimic prod deployment
    This is a simple Go server which sends requests to backends based on URL
    patterns. Essentially, this is a much simpler version of Envoy, implementing a
    tiny fraction of its capabilities with some standard libraries provided in Go.
    
    This is enough to route incoming requests either to our web UI server (Node) or
    the API server (another Go binary), and would mimic the actual production
    deployment if this were done in App Engine or Cloud Run, behind a reverse
    proxy which would direct requests similarly based on URL patterns.

---
+++ web/proxy/proxy.go
@@ -0,0 +1,137 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/ghodss/yaml"
+)
+
+var (
+	host = flag.String("host", "127.0.0.1", "Host to listen on; 127.0.0.1 only allows connections "+
+		"from localhost; set to 0.0.0.0 or empty to allow external connetions.")
+	port       = flag.Int("port", 9000, "Port to listen on")
+	configFile = flag.String("config", "", "Config file, either YAML (*.yaml, *.yml) or JSON (*.json) format.")
+)
+
+type Route struct {
+	Path   string `yaml:"path" json:"path"`
+	Target string `yaml:"target" json:"target"`
+}
+
+type ProxyConfig struct {
+	Routes []Route `yaml:"routes" json:"routes"`
+}
+
+type ConfigError struct {
+	Message string
+}
+
+func (ce *ConfigError) Error() string {
+	return ce.Message
+}
+
+func ParseConfig(filename string) (*ProxyConfig, error) {
+	if filename == "" {
+		return nil, &ConfigError{
+			Message: "Config filename must be specified (provided empty string)",
+		}
+	}
+
+	data, readErr := ioutil.ReadFile(filename)
+	if readErr != nil {
+		return nil, &ConfigError{
+			Message: fmt.Sprintf("Error reading file %s: %v", filename, readErr),
+		}
+	}
+
+	config := &ProxyConfig{}
+	if strings.HasSuffix(*configFile, "yaml") || strings.HasSuffix(*configFile, ".yml") {
+		yamlErr := yaml.Unmarshal(data, config)
+		if yamlErr != nil {
+			return nil, &ConfigError{
+				Message: fmt.Sprintf("Error parsing YAML: %v", yamlErr),
+			}
+		}
+	} else if strings.HasSuffix(*configFile, ".json") {
+		jsonErr := json.Unmarshal(data, config)
+		if jsonErr != nil {
+			return nil, &ConfigError{
+				Message: fmt.Sprintf("Error parsing JSON: %v", jsonErr),
+			}
+		}
+	} else {
+		return nil, &ConfigError{
+			Message: fmt.Sprintf("Unrecognized file suffix (only YAML and JSON are supported): %s\n", *config),
+		}
+	}
+	return config, nil
+}
+
+type Proxy struct {
+	Backends map[string]*httputil.ReverseProxy
+}
+
+func (proxy *Proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	log.Printf("Incoming request: %s", req.URL.Path)
+	for path, backend := range proxy.Backends {
+		if strings.HasPrefix(req.URL.Path, path) {
+			log.Printf("Redirecting %s\n", req.URL.Path)
+			backend.ServeHTTP(rw, req)
+			return
+		}
+	}
+	log.Printf("Error: unhandled request (no backend mapping): %s\n", req.URL)
+}
+
+func main() {
+	flag.Parse()
+
+	config, configErr := ParseConfig(*configFile)
+	if configErr != nil {
+		log.Printf("%v\n", configErr)
+		os.Exit(1)
+	}
+
+	backends := make(map[string]*httputil.ReverseProxy)
+	for _, route := range config.Routes {
+		target, err := url.Parse(route.Target)
+		if err != nil {
+			log.Fatalf("Error parsing target URL %s: %v\n", route.Target, err)
+		}
+		log.Printf("Redirecting %s -> %s\n", route.Path, route.Target)
+		backends[route.Path] = httputil.NewSingleHostReverseProxy(target)
+	}
+
+	hostPort := fmt.Sprintf("%s:%d", *host, *port)
+	log.Printf("Listening on %s", hostPort)
+
+	proxy := &Proxy{
+		Backends: backends,
+	}
+
+	http.Handle("/", proxy)
+	log.Fatal(http.ListenAndServe(hostPort, nil))
+}
